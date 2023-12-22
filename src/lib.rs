319a7d999cecfe979c58d11c1e4ddbca9ee243b8
Jay Barbhaiya <jay.barbhaiya@sap.com>
2023-12-22 10:01:58 +0100

    add extern function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-use extism_pdk::{plugin_fn, FnResult};
+use extism_pdk::{host_fn, plugin_fn, FnResult};
+
+#[host_fn("extism-plugin")]
+extern "ExtismHost" {
+    fn say_bye() -> String;
+}
 
 #[plugin_fn]
 pub fn greet(name: String) -> FnResult<String> {
     Ok(format!("Hello, {}!", name))
 }
-
